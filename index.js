cd06535cd6e638ce8224fbac00a8dada832ce136
Joan Marine <joanmarinecanals@gmail.com>
2022-08-25 23:33:12 +0200

    Cambios sobre bandas terminado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Socket } = require('dgram');
+
 const express = require('express');
-const path = require('path');
+const {resolve} = require('path');
 require('dotenv').config();
 
 // App de Express
@@ -11,18 +11,17 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server); 
 require('./sockets/socket');
 
-
+const {PORT} = process.env
 
 // Path público
-const publicPath = path.resolve( __dirname, 'public' );
-
+const publicPath = resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
 
 
-server.listen( process.env.PORT, ( err )=>{
+server.listen( PORT, ( err )=>{
 
     if ( err ) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT);
+    console.log('Servidor corriendo en puerto', PORT);
 
 });
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,22 +1,67 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Queen' ));
+bands.addBand( new Band( 'Estopa' ));
+bands.addBand( new Band( 'La Ludwigband' ));
+bands.addBand( new Band( 'Jazz Magnetism' ));
 
 
 //Mensajes de Sockets  
 io.on('connection', client => {
     console.log('Cliente conectado');
 
+    console.log(bands.getBands);
 
-    client.on('disconnect', () => { 
+    
+        client.emit('active-bands', bands.getBands());
+    
+
+    client.on('disconnect', () => {
         console.log('Cliente desconectado');
-     });
+    });
 
-     client.on('mensaje', ( payload )=>{
+    client.on('mensaje', (payload) => {
         console.log('Mensaje!!', payload);
 
-        io.emit( 'mensaje', {admin: 'Nuevo mensaje' });
+        io.emit('mensaje', { admin: 'Nuevo mensaje' });
 
-       
+    });
 
-     });
+    client.on('vote-band', (payload)=>{
 
-  });
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    //Escuchar add-band
+    client.on('add-band', (payload)=>{
+
+        const newBand = new Band(payload.name); 
+
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    //Delete band
+    client.on('delete-band', (payload)=>{
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     console.log('Mensaje:', payload);
+    //      //io.emit('nuevo-mensaje', payload);  //emite a todos
+    //     client.broadcast.emit('emitir -mensaje', payload);  //emite a todos menos al que emitió
+        
+    // } )
+
+});
